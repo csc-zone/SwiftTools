880c9e978fe0bc84959d44ebe67ff20d8706ba86
csc-zone <csc-zone@gmail.com>
2023-09-25 17:50:38 +0800

    fix

---
+++ Sources/SwiftTools/views/iconView.swift
@@ -0,0 +1,24 @@
+import SwiftUI
+
+public struct RectIconView: View {
+    
+    let name: String
+    let bg: Color
+    
+    public init(name: String, bg: Color) {
+        self.name = string
+        self.bg = bg
+    }
+    
+    public var body: some View {
+        Image(systemName: name)
+            .resizable()
+            .scaledToFit()
+            .font(.system(size: 12, weight: .medium))
+            .foregroundColor(Color.white)
+            .padding(6)
+            .background(bg)
+            .cornerRadius(8.0)
+            
+    }
+}
