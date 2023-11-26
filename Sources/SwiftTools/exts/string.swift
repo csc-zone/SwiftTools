de0455fac6b7a80aa8af8b994e320f3c3e8b2bd8
csc-zone <csc-zone@gmail.com>
2023-11-27 00:07:15 +0800

    fix

--- Sources/SwiftTools/exts/string.swift
+++ Sources/SwiftTools/exts/string.swift
@@ -130,7 +130,7 @@ public extension String {
         return count
     }
 
-    subscript (i: Int) -> String {
+    subscript(i: Int) -> String {
         return self[i ..< i + 1]
     }
 
@@ -142,11 +142,11 @@ public extension String {
         return self[0 ..< max(0, toIndex)]
     }
 
-    subscript (r: Range<Int>) -> String {
-        let range = Range(uncheckedBounds: (lower: max(0, min(length, r.lowerBound)),
-                                            upper: min(length, max(0, r.upperBound))))
-        let start = index(startIndex, offsetBy: range.lowerBound)
-        let end = index(start, offsetBy: range.upperBound - range.lowerBound)
-        return String(self[start ..< end])
-    }
+    // subscript(r: Range<Int>) -> String {
+    //     let range = Range(uncheckedBounds: (lower: max(0, min(length, r.lowerBound)),
+    //                                         upper: min(length, max(0, r.upperBound))))
+    //     let start = index(startIndex, offsetBy: range.lowerBound)
+    //     let end = index(start, offsetBy: range.upperBound - range.lowerBound)
+    //     return String(self[start ..< end])
+    // }
 }
